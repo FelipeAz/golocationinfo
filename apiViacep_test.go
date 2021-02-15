875a6dfd04d8d454bd75716dc052b1bcb0b598f5
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2021-02-15 19:06:27 -0300

    Update Repo

--- apiViacep_test.go
+++ apiViacep_test.go
@@ -11,12 +11,12 @@ func TestGetAPIData(t *testing.T) {
 		cep         string
 		cepExpected string
 	}{
-		{cep: "17123082", cepExpected: "17123-082"},
 		{cep: "17123-082", cepExpected: "17123-082"},
+		{cep: "17123082", cepExpected: "17123-082"},
 		{cep: "17015300", cepExpected: "17015-300"},
-		{cep: "", cepExpected: ""},
 		{cep: "11111111", cepExpected: ""},
 		{cep: "11111-111", cepExpected: ""},
+		{cep: "", cepExpected: ""},
 	}
 
 	for _, test := range tests {
