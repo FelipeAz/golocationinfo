09e251627401bbad881bc36a6054266d5ee5104b
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2021-02-15 08:56:44 -0300

    Readable test

--- apiViacep_test.go
+++ apiViacep_test.go
@@ -8,23 +8,23 @@ const defaultErrorMsg = "Expecting: %v. Found: %v."
 
 func TestGetAPIData(t *testing.T) {
 	tests := []struct {
-		cep      string
-		expected string
+		cep         string
+		cepExpected string
 	}{
-		{"17123082", "17123-082"},
-		{"17123-082", "17123-082"},
-		{"17015300", "17015-300"},
-		{"", ""},
-		{"11111111", ""},
-		{"11111-111", ""},
+		{cep: "17123082", cepExpected: "17123-082"},
+		{cep: "17123-082", cepExpected: "17123-082"},
+		{cep: "17015300", cepExpected: "17015-300"},
+		{cep: "", cepExpected: ""},
+		{cep: "11111111", cepExpected: ""},
+		{cep: "11111-111", cepExpected: ""},
 	}
 
 	for _, test := range tests {
 		t.Logf("Mass: %v", test)
 		result, _ := getAPIData(test.cep)
 
-		if result.Cep != test.expected {
-			t.Errorf(defaultErrorMsg, test.expected, result.Cep)
+		if result.Cep != test.cepExpected {
+			t.Errorf(defaultErrorMsg, test.cepExpected, result.Cep)
 		}
 	}
 }
